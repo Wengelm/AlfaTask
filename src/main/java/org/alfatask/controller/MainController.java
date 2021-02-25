3fc8ed7567c8ec420c480618c89407abe335b3ce
Pavel Zazulinski <wengelmcod@gmail.com>
2021-02-25 03:23:39 +0300

    Added Feign client for OpenExchangerates API Added model for OpenExchangerates API Added controller for get latest OpenExchangeRates API now MainController return random giphy from Giphy API when exchangeRate is checked.

--- src/main/java/org/alfatask/controller/MainController.java
+++ src/main/java/org/alfatask/controller/MainController.java
@@ -1,10 +1,15 @@
 package org.alfatask.controller;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import org.alfatask.OpenExchangeService;
+import org.alfatask.model.Giphy;
+import org.alfatask.model.GiphyResponse;
+import org.alfatask.service.ExchangeRateService;
+import org.alfatask.service.GiphyAPI;
+import org.alfatask.service.GiphyService;
+import org.alfatask.service.OpenExchangeAPI;
 import org.alfatask.model.OpenExchange;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +17,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
+
     @Autowired
-    private OpenExchangeService openExchangeService;
+    private ExchangeRateService exchangeRateService;
+    @Autowired
+    private GiphyService giphyService;
+    
 
     @RequestMapping("/")
-    public OpenExchange index(@RequestParam(required = true) String test)
+    public Giphy index()
 
     {
-        OpenExchange openExchange = openExchangeService.getOpenExchange(test);
-        System.out.println(openExchange);
-        return openExchange;
+
+        boolean isExchangeRateLarger = exchangeRateService.checkExchangeRate();
+
+        GiphyResponse ghResponse = giphyService.getGiphy(isExchangeRateLarger);
+
+
+
+        return ghResponse.data;
     }
 
 }
